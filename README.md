760a8da743ef5528e71160a6a607d0daebb0f4cb
Clodoaldo Lucio <clodoaldoulias@hotmail.com>
2024-11-26 22:29:46 -0300

    commit scripts 3 e 4

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Aprendendo mongo DB
 
 # Uso "#%%" por que é uma extensão do VSCode como jupyter notebook para separação do código em bloco para "debugar" erros 
+
+
+	ocorrencia_count	nome_região
+0	1364	            Nordeste
+1	1869	            Centro-Oeste
+2	751	                None
+3	5373	            Sudeste
+4	1410	            Norte
+5	2244	            Sul
---
+++ python/script_4_query1.py
@@ -0,0 +1,49 @@
+#%%
+from pymongo import MongoClient
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+#%%
+def lendo_mongo(db_name, collection_name, columns_exc=None, query=None, MONGODB_URI=os.getenv('URI')):
+    client = MongoClient(MONGODB_URI) #URI é a string de acesso ao banco
+    db = client.get_database(db_name) #Nome do database do MongoDb
+    collection = db.get_collection(collection_name) #Nome da collection que está no db e você usará
+    
+    #Definindo o cursor que executará a query
+    cursor = collection.aggregate(query)
+    
+    #Criando um df a partir da query e retornando ele
+    df = pd.DataFrame(list(cursor))
+    
+    return df
+
+def df_tratado(df):
+    
+    if "_id" in df.columns:
+        df.drop(columns=["_id"], inplace=True)
+        
+    if "Data_da_Ocorrencia" in df.columns:
+        df['Data_da_Ocorrencia'] = pd.to_datetime(df['Data_da_Ocorrencia'])
+    
+    return df   
+
+df_sem_tratar = lendo_mongo(
+    query = [{"$group":{
+        "_id": "$Regiao",
+        "ocorrencia_count":{"$sum":1},
+        "nome_região": {"$first": "$Regiao"}
+        }
+    }], #query é o filtro que desejo aplicar
+    columns_exc={}, #columns_exc é as colunas que desejo ou não desejo na consulta
+    db_name="Aula", #Nome do DB que usaremos
+    collection_name="anac" #nome da collections que tem no banco e usaremos
+)
+
+
+df_tratado(df_sem_tratar)
+
+#%%
