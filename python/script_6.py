0fff853eb117cf03f25f13d56b82994f961f788c
Clodoaldo Lucio <clodoaldoulias@hotmail.com>
2025-01-07 20:56:01 -0300

    refatorando e modificando script 6. Adicionado limpeza e tratamento de colunas do dataframe

--- python/script_6.py
+++ python/script_6.py
@@ -4,7 +4,7 @@ import pandas as pd
 from dotenv import load_dotenv
 import os
 import requests
-#%%
+
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
@@ -18,8 +18,7 @@ def get_api(api: str) -> pd.DataFrame:
     try:
         response = requests.get(api)
         if response.status_code == 200:
-            data = response.json()
-            df_data = pd.DataFrame(data)
+            df_data = pd.DataFrame(response.json())
             return df_data
         
     except requests.exceptions.RequestException as e:
@@ -34,7 +33,17 @@ def df_normalize(df_data: pd.DataFrame) -> pd.DataFrame:
     if "municipio" in df_data.columns:
         df_municipios = pd.json_normalize(df_data['municipio'], sep='_') #normalizando as colunas
         df_municipios.columns = [f"municipios_{col}" for col in df_municipios.columns] #renomeando as colunas do df normalizado para poder fazer o join dos dois df
-        df_all_columns = df_data.drop(columns=['municipio']).join(df_municipios)
+        df_all_columns = df_data.drop(columns=['municipio']).join(df_municipios.drop(columns=['municipios_regiao-imediata_regiao-intermediaria_UF_sigla',
+                                                                                              'municipios_regiao-imediata_regiao-intermediaria_UF_nome',
+                                                                                              'municipios_regiao-imediata_regiao-intermediaria_UF_regiao_sigla',
+                                                                                              'municipios_regiao-imediata_regiao-intermediaria_UF_regiao_nome',
+                                                                                              'municipios_regiao-imediata_regiao-intermediaria_UF_id',
+                                                                                              'municipios_regiao-imediata_regiao-intermediaria_UF_regiao_id']))
+        for col_name in df_all_columns.columns: #Renomeando colunas
+            if col_name.count('_') > 1:
+                partes = col_name.split('_')
+                df_all_columns = df_all_columns.rename(columns={col_name : f"{partes[-2]}_{partes[-1]}"})
+            
         return df_all_columns
     else:
         print("Erro ou tentar normalizar o arquivo, pois a coluna 'nome' não existe")
@@ -42,6 +51,6 @@ def df_normalize(df_data: pd.DataFrame) -> pd.DataFrame:
     
     
 data = get_api(url)
-df_normalize(data).head(15)
+df_normalize(data).head()
 
 #%%
